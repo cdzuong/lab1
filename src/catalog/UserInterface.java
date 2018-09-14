36a1a775a328ccd93469d575356cd499af617d9a
caseydzuong <cdzuong@gmail.com>
2018-09-14 13:43:06 -0700

    Revised User Interface file, now returns successful checkout or availability messages.

--- src/catalog/UserInterface.java
+++ src/catalog/UserInterface.java
@@ -29,35 +29,50 @@ public class UserInterface {
 
         Scanner input = new Scanner(System.in);
 
-        int repeat = 0;
+        String repeat;
 
         System.out.println("To check out a book, enter 1");
         System.out.println("To return a book, enter 2");
         System.out.println("To see a list of books currently available, enter 3");
         System.out.println("To exit, enter 4");
 
+        boolean checkedOut;
+
+
+        // figure out how to say if book with title available or not
+        // do same for return book - success message?
 
  String selection;
         do {
-            repeat = input.nextInt();
-            if (repeat == 1) {
+            repeat = input.nextLine();
+            if (repeat.equals("1")) {
                 System.out.println("Type in the title you'd like to check out then press 'Enter': ");
                 selection = input.nextLine();
-                lc.checkoutBook(selection);
+                checkedOut = lc.checkoutBook(selection);
+                if (checkedOut == true){
+                    System.out.println("Success!");
+                }
+                else{
+                    System.out.println("Unavailable, sorry.");
+                }
+
             }
-            else if (repeat == 2) {
+            else if (repeat.equals("2")) {
                 System.out.println("Type in the title of your book then press 'Enter'. If it is currently checked out, it will be returned.");
                 selection = input.nextLine();
                 lc.returnBook(selection);
             }
-            else if (repeat == 3) {
+            else if (repeat.equals("3")) {
                 System.out.println(lc.getAvailableBooks());
             }
-            else if (repeat == 4){
+
+
+
+            else if (repeat.equals("4")){
                 System.out.println("Program exited.");
                 break;
             }
-        } while (repeat != 4);
+        } while (!repeat.equals("4"));
 
         // If the user enters 1, ask for the title, and if the book is available,
         // check it out
